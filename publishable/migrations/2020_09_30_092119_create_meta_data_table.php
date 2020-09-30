338ffb9404d117758a76e21c7fd595845a168285
seyedmr <mr.smaili313@gmail.com>
2020-09-30 10:09:32 +0330

    first commit

---
+++ publishable/migrations/2020_09_30_092119_create_meta_data_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMetaDataTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meta_data', function (Blueprint $table) {
+            $table->id();
+            $table->string('model',32);
+            $table->string('key',32);
+            $table->text('value');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('meta_data');
+    }
+}
